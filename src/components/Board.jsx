aff4cd9155b51336c7c1cde1941aaf8bff5b624a
michaeloye <michaeloye23@gmail.com>
2022-04-20 22:49:13 +0100

    Add Cell component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Cell from "./Cell";
 
 function Board() {
-  return <div className="board"></div>;
+  return (
+    <div className="board">
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8].map(() => (
+        <Cell />
+      ))}
+    </div>
+  );
 }
 
 export default Board;
