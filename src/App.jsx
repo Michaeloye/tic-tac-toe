845ccd50b7a1c83199af714be197e2d003bd0443
michaeloye <michaeloye23@gmail.com>
2022-04-21 17:54:48 +0100

    And player score counter

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,29 @@ import Header from "./components/Header";
 
 function App() {
   const [whosTurn, setWhosTurn] = useState("1");
+  const [player1Score, setPlayer1Score] = useState(0);
+  const [player2Score, setPlayer2Score] = useState(0);
 
   const handleWhosTurn = (player) => {
     setWhosTurn(player);
   };
+  const handlePlayer1Score = () =>
+    setPlayer1Score((prevState) => prevState + 1);
+  const handlePlayer2Score = () =>
+    setPlayer2Score((prevState) => prevState + 1);
+
   return (
     <div className="App">
-      <Header whosTurn={whosTurn} />
-      <Board handleWhosTurn={handleWhosTurn} />
+      <Header
+        whosTurn={whosTurn}
+        player1Score={player1Score}
+        player2Score={player2Score}
+      />
+      <Board
+        handleWhosTurn={handleWhosTurn}
+        handlePlayer1Score={handlePlayer1Score}
+        handlePlayer2Score={handlePlayer2Score}
+      />
       <p className="turn-mobile">Player {whosTurn}'s Turn</p>
     </div>
   );
