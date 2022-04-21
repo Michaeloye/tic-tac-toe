7138c8e4824536c01b804d5ff77dc269cc464c10
michaeloye <michaeloye23@gmail.com>
2022-04-21 03:14:39 +0100

    Show Cell element based on props

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { BiCircle } from "react-icons/bi";
 
-function Cell() {
+function Cell({ isElementX, isElementO, onClick }) {
   return (
-    <button class="cell">
-      <BiCircle size={86} color={"white"} />
-      {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg> */}
+    <button className="cell" onClick={() => onClick()}>
+      {/* for X */}
+      {isElementO && <BiCircle size={86} color={"white"} />}
+
+      {/* for O */}
+      {isElementX && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="white"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M6 18L18 6M6 6l12 12"
+          ></path>
+        </svg>
+      )}
     </button>
   );
 }
