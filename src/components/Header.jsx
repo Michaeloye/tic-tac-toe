d82a3aa799406b86db71bfa8333e784d92728dca
michaeloye <michaeloye23@gmail.com>
2022-04-21 17:55:48 +0100

    Add player score prop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Score from "./Score";
 
-function Header({ whosTurn }) {
+function Header({ whosTurn, player1Score, player2Score }) {
   return (
     <>
       <br />
       <section className="header">
         {/* player1 and player2 scores */}
-        <Score player={1} score={10} />
+        <Score player={1} score={player1Score} />
         <p className="turn-desktop">
           Player {whosTurn}'s <span>Turn</span>
         </p>
-        <Score player={2} score={10} />
+        <Score player={2} score={player2Score} />
       </section>
     </>
   );
